817b69307e6da8340181961489a8aff2888e83b8
Golam Rabbani <golamrabbani9373@gmail.com>
2025-10-01 11:27:27 +0600

    logo and header customize complete

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -6,26 +6,39 @@ import Navbar from '../components/navbar/navbar'
 import FooterTop from '../components/footer-top'
 import Footer from '../components/footer'
 
-export default function Page() {
-  return (
-    <>
-        <Navbar transparent={false}/>   
+export default function NotFoundPage() {
+	return (
+		<>
+			<Navbar transparent={false} />
 
-        <section className="error-wrap">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-lg-6 col-md-10">
-                        <div className="text-center">
-                            <Image src="/img/404.png" width={0} height={0} sizes='100vw' style={{width:'100%', height:'auto'}} className="img-fluid" alt=""/>
-                            <p>Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi ultricies laoreet ullamcorper phasellus semper</p>
-                            <Link className="btn btn-primary px-5" href="/">Back To Home</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <FooterTop bg="theme-bg"/>
-        <Footer/>
-    </>
-  )
+			<section className="error-wrap">
+				<div className="container">
+					<div className="row justify-content-center">
+						<div className="col-lg-6 col-md-10">
+							<div className="text-center">
+								<Image
+									src="/img/404.png"
+									width={0}
+									height={0}
+									sizes="100vw"
+									style={{width: '100%', height: 'auto'}}
+									className="img-fluid"
+									alt=""
+								/>
+								<p>
+									Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi
+									ultricies laoreet ullamcorper phasellus semper
+								</p>
+								<Link className="btn btn-primary px-5" href="/">
+									Back To Home
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<FooterTop bg="theme-bg" />
+			<Footer />
+		</>
+	);
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import NotFoundPage from "./404/page";
+
+const NotFound = () => {
+  return <NotFoundPage />;
+};
+
+export default NotFound;
