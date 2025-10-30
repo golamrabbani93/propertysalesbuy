0f2349a160c6e4acbea14c92bf96f72151d7d14d
Golam Rabbani <golamrabbani9373@gmail.com>
2025-10-30 22:29:08 +0600

    add loader for dashboard

---
+++ src/app/components/Loader/DashboardLoader.jsx
@@ -0,0 +1,12 @@
+import './Loader.css';
+const DashboardLoader = () => {
+	return (
+		<div className="dashboard-wraper h-75 w-100 d-flex justify-content-center align-items-center">
+			<div className="dashboard-loader">
+				<span className="loader"></span>
+			</div>
+		</div>
+	);
+};
+
+export default DashboardLoader;
