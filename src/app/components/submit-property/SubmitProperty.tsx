f9e367786128d4cad443a869779f75967ccf2a27
Golam Rabbani <golamrabbani9373@gmail.com>
2025-11-04 20:45:33 +0600

    add post field

--- src/app/components/submit-property/SubmitProperty.tsx
+++ src/app/components/submit-property/SubmitProperty.tsx
@@ -15,9 +15,11 @@ import PSBTextArea from '../form/PSBTextArea';
 import {FieldValues} from 'react-hook-form';
 import PSBForm from '../form/PSBForm';
 import PropertySuccessModal from './PropertySuccessModal';
+import VideoUploader from '../VideoUploader/VideoUploader';
+import {APFeatureOptions, ApType, bedrooms, status} from './option';
 
 export default function SubmitProperty() {
-	// const [file, setFile] = useState('');
+	const [uploadedVideos, setUploadedVideos] = useState<IUploadedImage[]>([]);
 	const [uploadedImages, setUploadedImages] = useState<IUploadedImage[]>([]);
 	// function handleChange(e: any) {
 	// 	console.log(e.target.files);
@@ -27,85 +29,10 @@ export default function SubmitProperty() {
 	const {data: userData, isLoading} = useGetMyProfileQuery(user?.id);
 	const [createProperty, {isLoading: uploadLoading}] = useCreatePropertyMutation();
 	const [showModal, setShowModal] = useState(false);
-	const status = [
-		// {value: '1', label: 'For Rent'},
-		{value: 'for sell', label: 'For Sell'},
-	];
-	const type = [
-		{value: 'apartment', label: 'Apartment'},
-		{value: 'house', label: 'House'},
-		{value: 'villa', label: 'Villa'},
-		{value: 'land', label: 'Land'},
-	];
-	const bedrooms = [
-		{label: '1', value: '1'},
-		{label: '2', value: '2'},
-		{label: '3', value: '3'},
-		{label: '4', value: '4'},
-		{label: '5', value: '5'},
-	];
-	const featureOptions = [
-		{label: '1 Out of 5 Floors', value: '1 Out of 5 Floors'},
-		{label: '2 Out of 5 Floors', value: '2 Out of 5 Floors'},
-		{label: '3 Out of 5 Floors', value: '3 Out of 5 Floors'},
-		{label: '4 Out of 5 Floors', value: '4 Out of 5 Floors'},
-		{label: '5 Out of 5 Floors', value: '5 Out of 5 Floors'},
-
-		// Views & Orientation
-		{label: 'Road View', value: 'Road View'},
-		{label: 'Park View', value: 'Park View'},
-		{label: 'River View', value: 'River View'},
-		{label: 'Sea View', value: 'Sea View'},
-		{label: 'North Facing', value: 'North Facing'},
-		{label: 'South Facing', value: 'South Facing'},
-		{label: 'East Facing', value: 'East Facing'},
-		{label: 'West Facing', value: 'West Facing'},
-
-		// Facilities
-		{label: '1 Lift(s)', value: '1 Lift(s)'},
-		{label: '2 Lift(s)', value: '2 Lift(s)'},
-		{label: 'Private AC', value: 'Private AC'},
-		{label: 'Central AC', value: 'Central AC'},
-		{label: 'Substation Room', value: 'Substation Room'},
-		{label: 'Generator [Light,Fan]', value: 'Generator [Light,Fan]'},
-		{label: 'CCTV Security', value: 'CCTV Security'},
-		{label: 'Fire Safety', value: 'Fire Safety'},
-		{label: 'Gym', value: 'Gym'},
-		{label: 'Swimming Pool', value: 'Swimming Pool'},
-		{label: 'Community Hall', value: 'Community Hall'},
-
-		// Utilities
-		{label: 'Electricity', value: 'Electricity'},
-		{label: 'Water Supply', value: 'Water Supply'},
-		{label: 'Own Deep Tube', value: 'Own Deep Tube'},
-		{label: 'Cylinder Gas', value: 'Cylinder Gas'},
-		{label: 'Solar Water Heater', value: 'Solar Water Heater'},
-
-		// Parking & Roads
-		{label: '1 Car Parking', value: '1 Car Parking'},
-		{label: '2 Car Parking', value: '2 Car Parking'},
-		{label: '3 Car Parking', value: '3 Car Parking'},
-		{label: '20 Ft. Road', value: '20 Ft. Road'},
-		{label: '30 Ft. Road', value: '30 Ft. Road'},
-		{label: '40 Ft. Road', value: '40 Ft. Road'},
-		{label: '50 Ft. Road', value: '50 Ft. Road'},
-
-		// Furnishing
-		{label: 'Furnished', value: 'Furnished'},
-		{label: 'Semi-Furnished', value: 'Semi-Furnished'},
-		{label: 'Unfurnished', value: 'Unfurnished'},
-
-		// Others
-		{label: 'Balcony', value: 'Balcony'},
-		{label: 'Terrace', value: 'Terrace'},
-		{label: 'Garden', value: 'Garden'},
-		{label: 'Storage Room', value: 'Storage Room'},
-		{label: 'Servant Quarter', value: 'Servant Quarter'},
-	];
 
 	const handleSubmit = (data: FieldValues, method: any) => {
 		const toastId = toast.loading(
-			'Creating property... This may take a few moments to upload your images.',
+			'Creating property... This may take a few moments to upload your images and video.',
 			{
 				style: {
 					background: '#fff3e0', // soft orange background
@@ -142,8 +69,12 @@ export default function SubmitProperty() {
 			toast.error('Please upload at least 1 images', {id: toastId});
 			return;
 		}
-		if (uploadedImages.length > 4) {
-			toast.error('You can upload maximum 4 images', {id: toastId});
+		if (uploadedImages.length > 6) {
+			toast.error('You can upload maximum 6 images', {id: toastId});
+			return;
+		}
+		if (uploadedVideos.length < 1) {
+			toast.error('Please upload A video', {id: toastId});
 			return;
 		}
 
@@ -151,7 +82,10 @@ export default function SubmitProperty() {
 		uploadedImages.forEach((image, index) => {
 			formattedData[`image${index + 1}`] = image.file;
 		});
-
+		// append videos to data
+		uploadedVideos.forEach((video) => {
+			formattedData[`videos`] = video.file;
+		});
 		const formData = new FormData();
 		for (const key in formattedData) {
 			formData.append(key, formattedData[key]);
@@ -163,6 +97,7 @@ export default function SubmitProperty() {
 				setShowModal(true);
 				toast.success('Property Create Complete', {id: toastId});
 				setUploadedImages([]);
+				setUploadedVideos([]);
 				method.reset();
 			} else {
 				toast.error('Property Create Failed', {id: toastId});
@@ -202,7 +137,7 @@ export default function SubmitProperty() {
 								<PSBSelect
 									name="property_type"
 									label="Select Type"
-									options={type}
+									options={ApType}
 									placeholder="Select an option"
 								/>
 							</div>
@@ -210,8 +145,8 @@ export default function SubmitProperty() {
 							<div className="form-group col-md-6">
 								<PSBInput
 									name="price"
-									label="Property TAKA"
-									placeholder="Enter Property TAKA"
+									label="Property Price (TAKA)"
+									placeholder="Enter Property Price"
 									type="text"
 								/>
 							</div>
@@ -219,8 +154,8 @@ export default function SubmitProperty() {
 							<div className="form-group col-md-6">
 								<PSBInput
 									name="areas"
-									label="Property Areas"
-									placeholder="Enter Property Areas"
+									label="Property Areas (in sqft)"
+									placeholder="Enter Property Areas (in sqft)"
 									type="text"
 								/>
 							</div>
@@ -245,68 +180,25 @@ export default function SubmitProperty() {
 					<h3>Gallery</h3>
 					<div className="submit-section position-relative">
 						<div className="row">
-							<div className="form-group col-md-12">
+							<div className="form-group col-md-7">
 								<label>Upload Gallery</label>
-								{/* <form
-                            action="/upload-target"
-                            className="dropzone dz-clickable primary-dropzone"
-                          >
-                            <div className="position-absolute w-100 h-100 top-0 bottom-0">
-                              <input
-                                type="file"
-                                onChange={handleChange}
-                                style={{width: '100%', height: '100%', opacity: '0'}}
-                              />
-                            </div>
-                            {file && (
-                              <div className="dz-image">
-                                <img
-                                  src={file}
-                                  alt=""
-                                  style={{width: '120px', height: '120px', borderRadius: '15px'}}
-                                />
-                              </div>
-                            )}
-                            {!file && (
-                              <div className="dz-default dz-message">
-                                <i className="fa-solid fa-images"></i>
-                                <span>Drag & Drop To Change Logo</span>
-                              </div>
-                            )}
-                          </form> */}
 								<ImageUploader
 									uploadedImages={uploadedImages}
 									setUploadedImages={setUploadedImages}
-									maxImages={4}
+									maxImages={6}
+								/>
+							</div>
+							<div className="form-group col-md-5">
+								<label>Upload Video</label>
+								<VideoUploader
+									uploadedVideos={uploadedVideos}
+									setUploadedVideos={setUploadedVideos}
+									maxVideos={1}
 								/>
 							</div>
 						</div>
 					</div>
 				</div>
-
-				{/* <div className="form-submit">
-                    <h3>Location</h3>
-                    <div className="submit-section">
-                      <div className="row">
-                        <div className="form-group col-md-6">
-                          <label className="mb-2">Address</label>
-                          <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group col-md-6">
-                          <label className="mb-2">City</label>
-                          <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group col-md-6">
-                          <label className="mb-2">State</label>
-                          <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group col-md-6">
-                          <label className="mb-2">Zip Code</label>
-                          <input type="text" className="form-control" />
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
 
 				<div className="form-submit">
 					<h3>Detailed Information</h3>
@@ -324,7 +216,7 @@ export default function SubmitProperty() {
 								<PSBSelect
 									name="features"
 									label="Select Features"
-									options={featureOptions}
+									options={APFeatureOptions}
 									placeholder="Select features"
 									isMulti={true}
 								/>
