3662fadb67111ed56f7302871bac149e6f0b6ce3
Golam Rabbani <golamrabbani9373@gmail.com>
2025-11-04 20:41:39 +0600

    add my property inside dashboard

--- src/app/components/dashboard/property/MyProperty.tsx
+++ src/app/components/dashboard/property/MyProperty.tsx
@@ -3,14 +3,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {useState} from 'react';
 import DeleteModal from '../modal/delete-modal/DeleteModal';
-import {useDeletePropertyMutation} from '@/redux/features/property/propertyManagementApi';
+import {
+	useDeletePropertyMutation,
+	useUpdatePropertyMutation,
+} from '@/redux/features/property/propertyManagementApi';
 import {catchAsync} from '@/utils/catchAsync';
 import {toast} from 'sonner';
+import {useAppSelector} from '@/redux/hooks';
+import {selectUser} from '@/redux/features/auth/authSlice';
 
 const MyProperty = ({item}: {item: IProperty}) => {
 	const [showDeleteModal, setShowDeleteModal] = useState(false);
 	const [deleteProperty, {isLoading}] = useDeletePropertyMutation();
-
+	const [updatePropertyStatus] = useUpdatePropertyMutation();
+	const user = useAppSelector(selectUser);
 	const handleDelete = async () => {
 		catchAsync(async () => {
 			const res = await deleteProperty(item.id);
@@ -19,6 +25,23 @@ const MyProperty = ({item}: {item: IProperty}) => {
 				setShowDeleteModal(false);
 			} else {
 				toast.error('Property Delete Failed');
+			}
+		});
+	};
+
+	const handleMakePublished = async (data: string) => {
+		const toastId = toast.loading('Updating Property Status...');
+		const formatedData = {
+			title: item.title,
+			published: data === 'publish' ? true : false,
+		};
+		catchAsync(async () => {
+			// Assuming there's an API call to update the property status
+			const res = await updatePropertyStatus({id: item.id, data: formatedData});
+			if (res) {
+				toast.success(`Property marked as ${data} successfully`, {id: toastId});
+			} else {
+				toast.error('Failed to Update Property Status', {id: toastId});
 			}
 		});
 	};
@@ -45,7 +68,7 @@ const MyProperty = ({item}: {item: IProperty}) => {
 					</h4>
 					<div className="user_dashboard_listed">Price: from ৳ {item.price}</div>
 					<div className="user_dashboard_listed">
-						Property Type: <span className="text-primary">{item.property_type}</span>
+						Property Type: <span className="text-primary fw-bolder">{item.property_type}</span>
 					</div>
 					<div className="user_dashboard_listed">
 						Post Status:{' '}
@@ -56,27 +79,82 @@ const MyProperty = ({item}: {item: IProperty}) => {
 								<span className="text-danger fw-bolder">Unpublished</span>
 							)}
 						</span>
-					</div>
-					<div className="user_dashboard_listed">
-						Address: <span className="text-primary">{item.address}</span>
-					</div>
-					<div className="action">
-						<Link href={`/dashboard/my-property/${item.id}`} title="Edit">
-							<i className="fa-solid fa-pen-to-square"></i>
-						</Link>
-						<Link href="#" title="202 User View">
-							<i className="fa-regular fa-eye"></i>
-						</Link>
-						<Link
-							href="#"
-							title="Delete Property"
-							className="delete"
-							onClick={() => setShowDeleteModal(true)}
-						>
-							<i className="fa-regular fa-trash-can"></i>
-						</Link>
+						<div className="user_dashboard_listed">
+							Address:{' '}
+							<span className="text-primary">
+								{item.address?.length > 30 ? (
+									<>
+										{item.address.slice(0, 30)}
+										<br />
+										{item.address.slice(30)}
+									</>
+								) : (
+									item.address
+								)}
+							</span>
+						</div>
+						<div className="action">
+							{user?.role === 'admin' &&
+								(item.published ? (
+									<Link
+										href="#"
+										className="me-2"
+										onClick={() => handleMakePublished('unpublish')}
+										title="Unpublish"
+										target="_self"
+									>
+										<i className="fa-regular fa-circle-xmark"></i>
+									</Link>
+								) : (
+									<Link
+										href="#"
+										title="Make Published"
+										onClick={() => handleMakePublished('publish')}
+										target="_self"
+									>
+										<i className="fa-solid fa-check"></i>
+									</Link>
+								))}
+							<Link
+								href={`/${user?.role === 'admin' ? 'admin' : 'dashboard'}/my-property/${
+									item.property_type.toLowerCase() === 'land' ? 'land' : 'apartment'
+								}/${item.id}`}
+								title="Edit"
+							>
+								<i className="fa-solid fa-pen-to-square"></i>
+							</Link>
+							<Link href="#" title="202 User View">
+								<i className="fa-regular fa-eye"></i>
+							</Link>
+							<Link
+								href="#"
+								title="Delete Property"
+								className="delete"
+								onClick={() => setShowDeleteModal(true)}
+								target="_self"
+							>
+								<i className="fa-regular fa-trash-can"></i>
+							</Link>
+						</div>
 					</div>
 				</div>
+				{user?.role === 'admin' && (
+					<div className="sd-list-right bg-white  align-self-start">
+						<h4 className="listing_dashboard_title">Contact Information</h4>
+						<div className="user_dashboard_listed">
+							Name: <span className="text-primary fw-bolder">{item.contact_information.name}</span>
+						</div>
+						<div className="user_dashboard_listed">
+							Email:{' '}
+							<span className="text-primary fw-bolder">{item.contact_information.email}</span>
+						</div>
+
+						<div className="user_dashboard_listed">
+							Phone:{' '}
+							<span className="text-primary fw-bolder">{item.contact_information.phone}</span>
+						</div>
+					</div>
+				)}
 			</div>
 			<DeleteModal
 				show={showDeleteModal}
