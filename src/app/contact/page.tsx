4074fc34d2d4cb574b3fee1696a1cdc90a331cbb
Golam Rabbani <golamrabbani9373@gmail.com>
2025-11-06 21:09:25 +0600

    send normal message from contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+'use client';
 
 import Navbar from '../components/navbar/navbar';
 import FooterTop from '../components/footer-top';
 import Footer from '../components/footer';
 import ScrollToTop from '../components/scroll-to-top';
 import {Metadata} from 'next';
+import PSBForm from '../components/form/PSBForm';
+import {FieldValues} from 'react-hook-form';
+import PSBInput from '../components/form/PSBInput';
+import PSBTextArea from '../components/form/PSBTextArea';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {contactSendMessageSchema} from '@/schemas/sendMessage.schema';
+import {useCreateMessageMutation} from '@/redux/features/sendMessage/sendMessageManagementApi';
+import {catchAsync} from '@/utils/catchAsync';
+import {toast} from 'sonner';
+import MessageSuccessModal from '../components/MessageSuccessModal/MessageSuccessModal';
+import {useState} from 'react';
 
-export const metadata: Metadata = {
-	title: 'Contact Us - Propertysalesbuy',
-	description: 'Bangladesh Flat, House & Apartment Rental Platform',
-};
+// export const metadata: Metadata = {
+// 	title: 'Contact Us - Propertysalesbuy',
+// 	description: 'Bangladesh Flat, House & Apartment Rental Platform',
+// };
 export default function Page() {
+	const [show, setShow] = useState(false);
+	const [createMessage, {isLoading}] = useCreateMessageMutation();
+	const sendMessage = (formData: FieldValues, method: any) => {
+		const modifiedFormData = {
+			type: 'message',
+			...formData,
+		};
+		catchAsync(async () => {
+			const result = await createMessage(modifiedFormData).unwrap();
+			if (result?.id) {
+				toast.success('Message sent successfully');
+				setShow(true);
+				method.reset();
+			} else {
+				toast.error('Failed to send message');
+			}
+		});
+	};
 	return (
 		<>
 			<div className="page-title">
@@ -23,43 +52,48 @@ export default function Page() {
 					</div>
 				</div>
 			</div>
-
+			<MessageSuccessModal show={show} onClose={() => setShow(false)} />
 			<section>
 				<div className="container">
 					<div className="row">
 						<div className="col-lg-7 col-md-7">
-							<div className="row">
-								<div className="col-lg-6 col-md-6">
-									<div className="form-group">
-										<label className="mb-2">Name</label>
-										<input type="text" className="form-control simple" />
+							<PSBForm onSubmit={sendMessage} resolver={zodResolver(contactSendMessageSchema)}>
+								<div className="row">
+									<div className="col-lg-6 col-md-6">
+										<div className="form-group">
+											<PSBInput label="Name" name="name" type="text" />
+										</div>
+									</div>
+									<div className="col-lg-6 col-md-6">
+										<div className="form-group">
+											<PSBInput label="Email" name="email" type="email" />
+										</div>
 									</div>
 								</div>
-								<div className="col-lg-6 col-md-6">
-									<div className="form-group">
-										<label className="mb-2">Email</label>
-										<input type="email" className="form-control simple" />
+								<div className="row">
+									<div className="col-lg-6 col-md-6">
+										<div className="form-group">
+											<PSBInput label="Subject" name="subject" type="text" />
+										</div>
+									</div>
+									<div className="col-lg-6 col-md-6">
+										<div className="form-group">
+											<PSBInput label="Phone" name="phone" type="text" />
+										</div>
 									</div>
 								</div>
-							</div>
 
-							<div className="form-group">
-								<label className="mb-2">Subject</label>
-								<input type="text" className="form-control simple" />
-							</div>
+								<div className="form-group">
+									<PSBTextArea label="Message" name="message" />
+								</div>
 
-							<div className="form-group">
-								<label className="mb-2">Message</label>
-								<textarea className="form-control simple"></textarea>
-							</div>
-
-							<div className="form-group">
-								<button className="btn btn-primary px-5 rounded" type="submit">
-									Submit Request
-								</button>
-							</div>
+								<div className="form-group">
+									<button className="btn btn-primary px-5 rounded" type="submit">
+										{isLoading ? 'Sending...' : 'Send Message'}
+									</button>
+								</div>
+							</PSBForm>
 						</div>
-
 						<div className="col-lg-5 col-md-5">
 							<div className="contact-info">
 								<h2>Get In Touch</h2>
