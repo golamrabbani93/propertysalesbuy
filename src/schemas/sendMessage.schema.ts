3ded9e29932c7cb0869ca35b44a07c4007c5331a
Golam Rabbani <golamrabbani9373@gmail.com>
2025-11-06 21:10:11 +0600

    add send message schemas

--- src/schemas/sendMessage.schema.ts
+++ src/schemas/sendMessage.schema.ts
@@ -11,3 +11,20 @@ export const sendMessageSchema = z.object({
 		.min(10, 'Description is required')
 		.max(500, 'Description is required'),
 });
+
+export const contactSendMessageSchema = z.object({
+	name: z.string('Name is required').min(3, 'Name is required').max(50, 'Name is required'),
+	email: z.string('Email is required').email('Email is required'),
+	phone: z
+		.string('Phone number is required')
+		.min(5, 'Phone number is required')
+		.max(15, 'Phone number is required'),
+	message: z
+		.string('Description is required')
+		.min(10, 'Description is required')
+		.max(500, 'Description is required'),
+	subject: z
+		.string('Subject is required')
+		.min(5, 'Subject is required')
+		.max(100, 'Subject is required'),
+});
